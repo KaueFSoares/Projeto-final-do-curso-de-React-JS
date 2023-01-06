28d62370fcbe9fcd6a7adfc4af5f603aab439bc0
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-01-05 22:28:13 -0300

    Projects page criada com componente loading

--- projeto/projeto_final/src/components/pages/Projects.js
+++ projeto/projeto_final/src/components/pages/Projects.js
@@ -1,8 +1,17 @@
 import styles from "./Project.module.css"
 import Message from "../layout/Message"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
+import Loading from "../layout/Loading"
+
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
+
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
 
@@ -13,14 +22,67 @@ function Projects() {
         console.log(message)
     }
 
+
+    useEffect(() => {
+
+        setTimeout(
+            () => {
+                fetch("http://localhost:5000/projects", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                    .then(resp => resp.json())
+                    .then(data => {
+                        console.log(data)
+                        setProjects(data)
+                        setRemoveLoading(true)
+                    })
+                    .catch(err => console.log(err))
+            }, 500)
+    }, [])
+
+
+
+
+
     return (
         <div className={styles.box}>
-            <h1>My projects</h1>
+            <div className={styles.tittleContainer}>
+                <div>
+                    <h1>My projects</h1>
+                </div>
 
+                <div className={styles.linkButtonContainer}>
+                    <LinkButton to="/newproject" text="Create new project " />
+                </div>
+            </div>
             {message && <Message
                 msg={message}
                 type="success"
             />}
+
+            <div className={styles.container}>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            name={project.name}
+                            id={project.id}
+                            budget={project.budget}
+                            category={project?.category?.name}
+                            key={project.id}
+                        />
+                    ))}
+
+                {!removeLoading && <Loading />}
+
+                {removeLoading && projects.length === 0 &&
+
+                    <p>No projects</p>
+
+                }
+            </div>
         </div>
     )
 }
